e62feea925b40386a8f6378531587b84f04a686d
afnan030503 <muhammadafnanmaulidi@gmail.com>
2025-01-26 12:06:23 +0700

    membbuat inseert

---
+++ app/src/main/java/com/example/projectakhir/view/vendor/InsertViewVendor.kt
@@ -0,0 +1,138 @@
+package com.example.projectakhir.view.vendor
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.projectakhir.PenyediaViewModel
+import com.example.projectakhir.cmwidget.CustomTopAppBar
+import com.example.projectakhir.navigasi.DestinasiNavigasi
+import com.example.projectakhir.viewmodel.vendor.InsertVendorUiEvent
+import com.example.projectakhir.viewmodel.vendor.InsertVendorUiState
+import com.example.projectakhir.viewmodel.vendor.InsertViewModelVendor
+import com.example.projectakhir.viewmodel.vendor.JenisVendorRadioButton
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.selects.select
+
+object DestinasiVendorEntry : DestinasiNavigasi {
+    override val route = "entry_vendor"
+    override val titleRes = "Tambah Vendor"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryVendorScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertViewModelVendor = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CustomTopAppBar(
+                title = DestinasiVendorEntry.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ) { innerPadding ->
+        EntryVendorBody(
+            insertUiState = viewModel.uiState,
+            onVendorValueChange = viewModel::updateInsertVendorState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertVendor()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+fun EntryVendorBody(
+    insertUiState: InsertVendorUiState,
+    onVendorValueChange: (InsertVendorUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ) {
+        FormInputVendor(
+            insertUiEvent = insertUiState.insertUiEvent,
+            onValueChange = onVendorValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Simpan")
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInputVendor(
+    insertUiEvent: InsertVendorUiEvent,
+    modifier: Modifier = Modifier,
+    onValueChange: (InsertVendorUiEvent) -> Unit = {},
+    enabled: Boolean = true
+) {
+    Column(
+        modifier = Modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        OutlinedTextField(
+            value = insertUiEvent.nama_vendor,
+            onValueChange = { onValueChange(insertUiEvent.copy(nama_vendor = it)) },
+            label = { Text("Nama Vendor") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        JenisVendorRadioButton(
+            selectedJenisVendor = insertUiEvent.jenis_vendor,
+            onJenisVendorChange = { selected ->
+                onValueChange(insertUiEvent.copy(jenis_vendor = selected))
+            }
+        )
+        OutlinedTextField(
+            value = insertUiEvent.kontak_vendor,
+            onValueChange = { onValueChange(insertUiEvent.copy(kontak_vendor = it)) },
+            label = { Text("Kontak Vendor") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+
+        if (enabled) {
+            Text(
+                text = "Isi Semua Data!",
+                modifier = Modifier.padding(12.dp)
+            )
+        }
+        Divider(
+            thickness = 8.dp,
+            modifier = Modifier.padding(12.dp)
+        )
+    }
+}
---
+++ app/src/main/java/com/example/projectakhir/viewmodel/vendor/InsertViewModelVendor.kt
@@ -0,0 +1,113 @@
+package com.example.projectakhir.viewmodel.vendor
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.projectakhir.model.Vendor
+import com.example.projectakhir.repository.VendorRepository
+import kotlinx.coroutines.launch
+
+class InsertViewModelVendor(private val vendorRepo: VendorRepository) : ViewModel() {
+    var uiState by mutableStateOf(InsertVendorUiState())
+        private set
+
+    fun updateInsertVendorState(insertUiEvent: InsertVendorUiEvent) {
+        uiState = InsertVendorUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun insertVendor() {
+        viewModelScope.launch {
+            try {
+                vendorRepo.insertVendor(uiState.insertUiEvent.toVendor())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
+
+@Composable
+fun JenisVendorRadioButton(
+    selectedJenisVendor: String,
+    onJenisVendorChange: (String) -> Unit
+) {
+    // Semua nilai enum JenisVendor
+    val jenisVendorOptions = JenisVendor.values()
+
+    Column {
+        Text(
+            text = "Jenis Layanan Vendor",
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+
+        // Menampilkan RadioButton untuk setiap pilihan jenis vendor
+        jenisVendorOptions.forEach { jenisVendor ->
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 4.dp)
+                    .clickable { onJenisVendorChange(jenisVendor.displayNameVendor) }
+            ) {
+                RadioButton(
+                    selected = selectedJenisVendor == jenisVendor.displayNameVendor,
+                    onClick = { onJenisVendorChange(jenisVendor.displayNameVendor) }
+                )
+                Text(
+                    text = jenisVendor.displayNameVendor,
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(start = 8.dp)
+                )
+            }
+        }
+    }
+}
+
+data class InsertVendorUiState(
+    val insertUiEvent: InsertVendorUiEvent = InsertVendorUiEvent()
+)
+
+data class InsertVendorUiEvent(
+    val id_vendor: Int = 0,
+    val nama_vendor: String = "",
+    val jenis_vendor: String = JenisVendor.CATERING.displayNameVendor, // Defaultnya bisa CATERING
+    val kontak_vendor: String = ""
+)
+
+fun Vendor.toUiStateVendor(): InsertVendorUiState = InsertVendorUiState(
+    insertUiEvent = toInsertVendorUiEvent()
+)
+
+fun InsertVendorUiEvent.toVendor(): Vendor = Vendor(
+    id_vendor = id_vendor,
+    nama_vendor = nama_vendor,
+    jenis_vendor = jenis_vendor,
+    kontak_vendor = kontak_vendor
+)
+
+fun Vendor.toInsertVendorUiEvent(): InsertVendorUiEvent = InsertVendorUiEvent(
+    id_vendor = id_vendor,
+    nama_vendor = nama_vendor,
+    jenis_vendor = jenis_vendor,
+    kontak_vendor = kontak_vendor
+)
+enum class JenisVendor(val displayNameVendor: String) {
+    CATERING("Katering"),
+    DECORATION("Dekorasi"),
+    AUDIO_VISUAL("Audio Visual"),
+    TRANSPORTATION("Dll")
+}
+
